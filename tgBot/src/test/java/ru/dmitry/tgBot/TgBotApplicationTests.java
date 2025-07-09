f4616f0f2dde7f46f78b31a9c903211641dffb3b
Dmitry <fedyashevdm@gmail.com>
2025-07-09 14:32:30 +0300

    Практическое задание 2.

---
+++ tgBot/src/test/java/ru/dmitry/tgBot/TgBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dmitry.tgBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
