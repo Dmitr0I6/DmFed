ab86212037b00698a8093ea851020638b38ca99e
Dmitry <fedyashevdm@gmail.com>
2025-07-14 13:28:21 +0300

    ПЗ 3. С исправлениями

---
+++ tgBot/src/main/java/ru/dmitry/tgBot/ClientRepository.java
@@ -0,0 +1,8 @@
+package ru.dmitry.tgBot;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "clients",path = "clients")
+public interface ClientRepository extends JpaRepository<Client,Long> {
+}
---
+++ tgBot/src/main/java/ru/dmitry/tgBot/Product.java
@@ -0,0 +1,101 @@
+package ru.dmitry.tgBot;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne
+    private Category category;
+
+    @Column(length = 50,nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private Double price;
+
+    public Product(String name, Long id, Category category, String description, Double price) {
+        this.name = name;
+        this.id = id;
+        this.category = category;
+        this.description = description;
+        this.price = price;
+    }
+
+    public Product() {
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", category=" + category +
+                ", name='" + name + '\'' +
+                ", desctipton='" + description + '\'' +
+                ", price=" + price +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return id!= null && id.equals(product.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
